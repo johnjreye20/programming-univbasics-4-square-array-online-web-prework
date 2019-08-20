9a76012bd170ea1c73538ce4f8b259b948ea7988
John Reyes <john.jreye@gmail.com>
2019-08-20 02:23:38 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+ counter = 0
+ array = ["Left", "Right", "Up", "Down"]
+ 
+ new_array = ["North", "South", "East", "West"]
+ 
+ while array [counter] do 
+   puts array[counter]
+   counter += 1
 end
